01ee58c4b06ee0d98f0ad441cbaa384840b0410c
Lulu-Soso <71519777+Lulu-Soso@users.noreply.github.com>
2021-03-25 21:29:41 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,8 +1,7 @@
-
-<div class="container">
-    
-    <p class="inf">Merci <?php echo $_POST['firstname'];?> <?php echo $_POST['lastname'];?> de nous avoir contacté à propos de <?php echo $_POST['sujet'];?>.
-    Un de nos conseiller vous contactera soit à l’adresse: <?php echo $_POST['courriel'];?> ou par téléphone au <?php echo $_POST['phone'];?> 
-    dans les plus brefs délais pour traiter votre demande :</p>
-
-</div>
+<?php
+echo  'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] . ' de nous avoir contacté à propos de "' . $_POST['sujet'] . '”.';
+echo '<br>';
+echo 'Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['courriel'] . ' ou par téléphone au ' . $_POST['phone'] . ' dans les plus brefs délais pour traiter votre demande :';
+echo '<br>';
+echo $_POST['message'];
+?>
