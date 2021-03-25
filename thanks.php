fa7f81098ede51417bea5179b130ccbc5146317f
Lulu-Soso <71519777+Lulu-Soso@users.noreply.github.com>
2021-03-25 18:32:12 +0100

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,8 @@
+
+<div class="container">
+    
+    <p class="inf">Merci <?php echo $_POST['firstname'];?> <?php echo $_POST['lastname'];?> de nous avoir contacté à propos de <?php echo $_POST['sujet'];?>.
+    Un de nos conseiller vous contactera soit à l’adresse: <?php echo $_POST['courriel'];?> ou par téléphone au <?php echo $_POST['phone'];?> 
+    dans les plus brefs délais pour traiter votre demande :</p>
+
+</div>
